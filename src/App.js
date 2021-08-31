ac7727d8b000ca4c0628e32e82910d7b5b3679dc
Alex Pileggi <apileggi85@gmail.com>
2021-08-31 14:39:37 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
 
@@ -11,5 +12,11 @@ class App extends Component {
         );
     };
 }
+
+<BrowserRouter>
+    <div className="App">
+        <Main />
+    </div>
+</BrowserRouter>
 
 export default App;
